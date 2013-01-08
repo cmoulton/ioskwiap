79cce74322c770339369a15949cd0ce88cefb018
Christina <christina.moulton@gmail.com>
2013-01-08 12:56:14 -0500

    Set up buttons to purchase and check status of item

--- iOSKW IAP Test 1/IAPViewController.m
+++ iOSKW IAP Test 1/IAPViewController.m
@@ -7,8 +7,42 @@
 //
 
 #import "IAPViewController.h"
+#import "MKStoreManager.h"
+
+#define PRODUCT_1_ID @"com.teakmobile.ioskw.inappdemo.test1.product1"
 
 @implementation IAPViewController
 
+- (IBAction)purchaseProduct1
+{
+  [[MKStoreManager sharedManager] buyFeature:PRODUCT_1_ID
+                                  onComplete:^(NSString* purchasedFeature,
+                                               NSData* purchasedReceipt,
+                                               NSArray* downloads) {
+                                    NSLog(@"Purchased: %@", purchasedFeature);
+                                    // provide product to the user
+                                  } onCancelled:^ {
+                                    NSLog(@"User Cancelled Transaction");
+                                  }];
+}
+
+- (IBAction)showIsProduct1Purchased
+{
+  NSString *title;
+  if ([MKStoreManager isFeaturePurchased:PRODUCT_1_ID])
+  {
+    title = @"Product 1 is purchased";
+  }
+  else
+  {
+    title = @"Product 1 is not purchased";
+  }
+  UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"Product 1"
+                                                      message:title
+                                                     delegate:nil
+                                            cancelButtonTitle:@"OK"
+                                            otherButtonTitles:nil];
+  [alertview show];
+}
 
 @end
